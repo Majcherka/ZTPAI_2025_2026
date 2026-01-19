ed40acd4166481fcd46c7b815f9b87d535e8aa81
Majcherka <magdulka2003@gmail.com>
2026-01-19 17:53:37 +0100

    feat: konfiguracja routingu react-router-dom dla podstron

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,20 @@
-import Listings from "./components/Listings";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import MyNavbar from './components/MyNavbar';
+import Listings from './components/Listings';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
-    <div className="App">
-      <Listings />
-    </div>
+    <Router>
+      <MyNavbar />
+      <Routes>
+        <Route path="/" element={<Listings />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        {/* Tu zaraz dodamy dodawanie ogłoszeń */}
+      </Routes>
+    </Router>
   );
 }
 
