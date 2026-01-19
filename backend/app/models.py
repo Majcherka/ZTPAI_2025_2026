e6b21763fe82add9e3f8e129a76c2cff3cef17a4
Majcherka <magdulka2003@gmail.com>
2026-01-19 20:40:17 +0100

    feat: backend - dodanie tabeli asocjacyjnej favorites w bazie danych

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,27 +1,35 @@
-from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, Text, Float
+from sqlalchemy import Boolean, Column, Integer, String, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship
 from app.database import Base
+
+favorites_table = Table(
+    'favorites',
+    Base.metadata,
+    Column('user_id', Integer, ForeignKey('users.id'), primary_key=True),
+    Column('listing_id', Integer, ForeignKey('listings.id'), primary_key=True)
+)
 
 class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True, nullable=False)
-    hashed_password = Column(String, nullable=False)
-    role = Column(String, default="USER")
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
     is_active = Column(Boolean, default=True)
+    role = Column(String, default="USER")
 
     listings = relationship("Listing", back_populates="owner")
+    favorites = relationship("Listing", secondary=favorites_table, back_populates="favorited_by")
 
 class Listing(Base):
     __tablename__ = "listings"
 
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, index=True, nullable=False)
-    description = Column(Text, nullable=True)
-    city = Column(String, index=True, nullable=False)
-    price = Column(Float, nullable=False)
-    
+    title = Column(String, index=True)
+    description = Column(String)
+    city = Column(String, index=True)
+    price = Column(Float)
     user_id = Column(Integer, ForeignKey("users.id"))
 
     owner = relationship("User", back_populates="listings")
+    favorited_by = relationship("User", secondary=favorites_table, back_populates="favorites")
